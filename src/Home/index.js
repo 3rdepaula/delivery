8af6129034e30ac06de8226a5919c3a7da6c881c
Ramon de Paula <ramonsobreiradepaula@gmail.com>
2021-01-24 15:43:48 -0300

    feat: :lipstick: add Search component in Home

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,14 +1,59 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Text } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { SearchBar } from 'react-native-elements'
 
-import { Container } from './styles'
+import {
+  Container,
+  ContainerHeader,
+  ContainerName,
+  Figure,
+  ContainerNames,
+  TextWelcome,
+  Name,
+  Main,
+  ContainerCategories
+} from './styles'
 
 const Home = () => {
   return (
     <Container>
-      <View>
-        <Text>Header</Text>
-      </View>
+      <ContainerHeader>
+        <ContainerName>
+          <Figure>
+            <Text>😎</Text>
+          </Figure>
+          <ContainerNames>
+            <TextWelcome>Welcome back</TextWelcome>
+            <Name>Nikusha</Name>
+          </ContainerNames>
+        </ContainerName>
+        <Feather name='align-right' size={28} />
+      </ContainerHeader>
+      <Main>
+        <SearchBar
+          platform='android'
+          placeholder='Search'
+          placeholderTextColor='#979899'
+          onChangeText={(value) => null}
+          keyboardAppearance='light'
+          value={null}
+          inputStyle={{
+            color: '#000',
+            fontSize: 15,
+            fontFamily: 'Poppins500Medium'
+          }}
+          containerStyle={{
+            borderRadius: 15,
+            height: 50,
+            backgroundColor: '#f3f5f7',
+            marginHorizontal: 10,
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        />
+        <ContainerCategories />
+      </Main>
     </Container>
   )
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -4,4 +4,48 @@ import { StatusBar } from 'react-native'
 export const Container = styled.View`
   flex: 1;
   padding-top: ${StatusBar.currentHeight || 40}px;
+  background-color: ${props => props.theme.background};
 `
+
+export const ContainerHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 5px;
+  margin-right: 5px;
+`
+
+export const ContainerName = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Figure = styled.View`
+  background-color: lightblue;
+  height: 50px;
+  width: 50px;
+  border-radius: 25px;
+  align-items: center;
+  justify-content: center;
+`
+
+export const ContainerNames = styled.View`
+  margin-left: 10px;
+`
+
+export const TextWelcome = styled.Text`
+  font-family: 'Poppins600SemiBold';
+  opacity: 0.5;
+`
+
+export const Name = styled.Text`
+  font-family: 'Poppins600SemiBold';
+  font-size: 16px;
+`
+
+export const Main = styled.View`
+  margin-top: 30px;
+`
+
+export const ContainerCategories = styled.View``

ac35931a26eac1e67bc54cbef214c2099ba4c99f
Ramon de Paula <ramonsobreiradepaula@gmail.com>
2021-01-24 14:44:31 -0300

    feat: :lipstick: add fontFamily

--- App.js
+++ App.js
@@ -2,12 +2,29 @@ import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import Routes from './src/routes'
+import {
+  Poppins_600SemiBold as Poppins600SemiBold,
+  Poppins_400Regular as Poppins400Regular,
+  useFonts
+} from '@expo-google-fonts/poppins'
+import AppLoading from 'expo-app-loading'
 
-export default function App () {
-  return (
-    <>
-      <Routes />
-      <StatusBar style='auto' />
-    </>
-  )
+const App = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins600SemiBold,
+    Poppins400Regular
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <>
+        <Routes />
+        <StatusBar style='auto' />
+      </>
+    )
+  }
 }
+
+export default App

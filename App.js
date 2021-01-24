cf379de17f8f05c9e49fc92d4300deaff9e5c473
Ramon de Paula <ramonsobreiradepaula@gmail.com>
2021-01-24 15:36:23 -0300

    feat: :lipstick: add ThemeProvider and font Poppins500Medium

--- App.js
+++ App.js
@@ -5,24 +5,28 @@ import Routes from './src/routes'
 import {
   Poppins_600SemiBold as Poppins600SemiBold,
   Poppins_400Regular as Poppins400Regular,
+  Poppins_500Medium as Poppins500Medium,
   useFonts
 } from '@expo-google-fonts/poppins'
 import AppLoading from 'expo-app-loading'
+import { ThemeProvider } from 'styled-components'
+import colors from './styles'
 
 const App = () => {
   const [fontsLoaded] = useFonts({
     Poppins600SemiBold,
-    Poppins400Regular
+    Poppins400Regular,
+    Poppins500Medium
   })
 
   if (!fontsLoaded) {
     return <AppLoading />
   } else {
     return (
-      <>
+      <ThemeProvider theme={colors}>
         <Routes />
         <StatusBar style='auto' />
-      </>
+      </ThemeProvider>
     )
   }
 }
